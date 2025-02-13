47e5867d44caf917795be518f73787918b78e44f
Daryl Capa <129023363+sdbeth@users.noreply.github.com>
2025-02-13 17:04:09 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Learning C++ 
-// Exercise 00_03
+// Exercise 00_03 with modifications
 // Using the exercise files on GitHub, by Eduardo Corpeño 
 
 #include <iostream>
@@ -8,7 +8,7 @@ int main(){
     float num_1, num_2, result;
 
     std::cout << "Enter number 1: " << std::flush;
-    std::cin >> num_1;
+    std::cin >> num_1; 
     std::cout << "Enter number 2: " << std::flush;
     std::cin >> num_2;
     
